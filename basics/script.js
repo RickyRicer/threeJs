e6afb5a7b1891440f83d91f968baa3126a0f22db
Rick Ansay <rick.ansay@gmail.com>
2022-10-06 22:05:39 -0600

    Restructure directory

--- basics/script.js
+++ basics/script.js
@@ -1,5 +1,6 @@
 console.log(THREE);
 
+
 // Scene
 const scene = new THREE.Scene();
 
@@ -18,10 +19,14 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
 scene.add(camera);
 
-// Renderer  
+// Renderer
+const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas
 });
 renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
